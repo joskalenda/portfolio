7cccba373b24e06a7150de7667c6362e422b6f41
joskalenda <joskalenda3@gmail.com>
2021-12-13 18:59:41 +0200

    Fix linter errors

--- script.js
+++ script.js
@@ -3,24 +3,19 @@ const navHumburger = document.querySelector('.nav-humburger');
 const xIcon = document.querySelector('.xicon-image');
 const overlayNav = document.querySelectorAll('.overlay-link');
 
-
-function displayOn(){
+function displayOn() {
   navOverlay.classList.remove('off-display');
   document.body.classList.add('scroll');
 }
 
-
-function displayOff(){
+function displayOff() {
   navOverlay.classList.add('off-display');
   document.body.classList.remove('scroll');
-  for (let j=0; j < overlayNav.length; j+=1){
-    overlayNav[j].addEventListener('click',displayOff);
+  for (let j = 0; j < overlayNav.length; j += 1) {
+    overlayNav[j].addEventListener('click', displayOff);
   }
 }
 
-
-navHumburger.addEventListener('click',displayOn);
-xIcon.addEventListener('click',displayOff);
-overlayNav.addEventListener('click',displayOff);
-
-
+navHumburger.addEventListener('click', displayOn);
+xIcon.addEventListener('click', displayOff);
+overlayNav.addEventListener('click', displayOff);
