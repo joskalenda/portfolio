5de381b34db6bd8c347a3dc38cefb3290288c16c
joskalenda <joskalenda3@gmail.com>
2021-12-17 01:29:55 +0200

    Change eror color

--- js/validation.js
+++ js/validation.js
@@ -1,14 +1,14 @@
 const form = document.querySelector('#form');
 const email = document.querySelector('#email');
 const errorContainer = document.querySelector('.error-msg');
-const text = 'Enter lower case then try again';
+const text = 'Put email in lower case and try again';
 const message = document.createElement('h4');
 errorContainer.appendChild(message);
 message.innerText = text;
-message.style.color = 'red';
-message.style.background = 'white';
-message.style.fontWeight = '400';
-message.style.fontSize = '17px';
+message.style.color = '#36b37e';
+message.style.background = '#fff';
+message.style.fontWeight = '600';
+// message.style.fontSize = '17px';
 message.style.visibility = 'hidden';
 
 form.addEventListener('submit', (e) => {
